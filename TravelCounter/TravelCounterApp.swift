faa100cd78d98d490dc41dfe61dd96be5171b573
kaka0913 <kabuyuu0913@gmail.com>
2025-01-29 22:51:37 +0900

    git追跡に再度追加

---
+++ TravelCounter/TravelCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelCounterApp.swift
+//  TravelCounter
+//
+//  Created by 株丹優一郎 on 2025/01/29.
+//
+
+import SwiftUI
+
+@main
+struct TravelCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
